3bd65e5428ff9d2c1d1bad8656ce792775c27c6f
Victor Bejarano <alejandrobjrn4@gmail.com>
2021-06-19 15:43:48 -0500

    FlutterFire was implemented

--- android/app/src/main/kotlin/com/example/shopping_cart/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/shopping_cart/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.shopping_cart
+package com.victorbejarano.shopping_cart
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,44 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:shopping_cart/injections/injections.dart' as di;
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
-    );
+    return FutureBuilder<FirebaseApp>(
+        future: _initialization,
+        builder: (BuildContext context, AsyncSnapshot<FirebaseApp> snapshot) {
+          if (snapshot.hasError) {
+            // TODO: Crear vista cuando hay errores en la inicializacion
+            return Container();
+          }
+
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Material App',
+              home: Scaffold(
+                appBar: AppBar(
+                  title: Text('Material App Bar'),
+                ),
+                body: Center(
+                  child: Container(
+                    child: Text('Hello World'),
+                  ),
+                ),
+              ),
+            );
+          }
+
+          // TODO: Crear vista cuando se esta cargando
+          return Container();
+        });
   }
 }
